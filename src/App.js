4c9fced7e30cca17a2aa020d783736dd462d2eb9
KISHORE NANDHAN <nandhakishorejajula@gmail.com>
2024-08-08 22:00:36 +0530

    added icons to the navbar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import './index.css';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.js'
 import Search from './pages/Search.js'
 import Post from './pages/Post.js'
 import Profile from './pages/Profile.js'
-
 function App() {
   return (
     <Router>
-    <div className="App h-screen flex">
+    <div className='App h-screen flex'>
       <Navbar />
       <div className="flex-grow p-6">
         <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/search" element={<Search />} />
           <Route path="/post" element={<Post />} />
           <Route path="/profile" element={<Profile />} />
