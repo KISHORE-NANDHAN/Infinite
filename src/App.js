1f68ebc3a8334e3dc4c49ac4a990088d2f69e089
KISHORE NANDHAN <nandhakishorejajula@gmail.com>
2024-09-18 20:49:42 +0530

    added all files for backup

--- src/App.js
+++ src/App.js
@@ -1,33 +1,58 @@
-import React, { useContext } from 'react';
-import { Routes, Route} from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { UserProvider } from './context/UserContext.js';
-import { ThemeProvider} from './context/ThemeContext.js';
+import { ThemeProvider } from './context/ThemeContext.js';
 import Navbar from './components/Navbar';
 import Home from './pages/Home.js';
 import Search from './pages/Search.js';
 import Post from './pages/Post.js';
 import Profile from './pages/Profile.js';
 import NotFound from './pages/NotFound.js';
+import axios from 'axios';
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    // Check if the user is logged in by calling an API (modify the URL as needed)
+    axios.get('http://localhost:5000/api/session', { withCredentials: true })
+      .then(res => {
+        setLoggedIn(res.data.loggedIn);
+      })
+      .catch(() => {
+        setLoggedIn(false); // Set to false if error occurs or not logged in
+      });
+  }, []);
+
   return (
     <UserProvider>
       <ThemeProvider> 
-      <div className='App h-screen flex'>
-      <Navbar />
-      <div className="flex-grow p-6">
-        <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="/search" element={<Search />} />
-          <Route path="/post" element={<Post />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </div>
-    </div>
+        <div className='App h-screen flex'>
+          <Navbar />
+          <div className="flex-grow p-6">
+            <Routes>
+              <Route 
+                path="/home" 
+                element={loggedIn ? <Home /> : <Navigate to="/login" />} 
+              />
+              <Route 
+                path="/search" 
+                element={loggedIn ? <Search /> : <Navigate to="/login" />} 
+              />
+              <Route 
+                path="/post" 
+                element={loggedIn ? <Post /> : <Navigate to="/login" />} 
+              />
+              <Route 
+                path="/profile" 
+                element={loggedIn ? <Profile /> : <Navigate to="/login" />} 
+              />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </div>
+        </div>
       </ThemeProvider>
     </UserProvider>
-    
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.js'
-import Signup from './pages/Signup.js';
-import Login from './pages/Login.js';
-import Enter from './pages/Enter.js';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import NotFound from './pages/NotFound.js'
+import App from './App.js';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <Routes>
-        <Route path="/" element={<Enter />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path = "/login" element={<Login/>} />
-        <Route path='/app/*' element={<App/>} />
-        <Route path='*' element={<NotFound/>} />
-      </Routes>
+      <App />
     </Router>
   </React.StrictMode>
 );
